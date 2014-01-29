382fbcec96787a6e65ff6a5cfb57191689c3b46d
mojiAm <amjadi.mojtaba@gmail.com>
2014-01-29 10:00:59 -0800

    add header

--- themes/main.php
+++ themes/main.php
@@ -1,154 +1,6 @@
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" dir="rtl" lang="fa-IR">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="shortcut icon" href="./favicon.ico">
-	<title>Arshia</title>
-	<link rel="alternate" type="application/rss+xml" title="Sentinel » Feed" href="http://rawofnature.com/demfile/sn/wp/feed/">
-	<link rel="alternate" type="application/rss+xml" title="Sentinel » Comments Feed" href="http://rawofnature.com/demfile/sn/wp/comments/feed/">
-	<link rel="stylesheet" id="flick-css" href="http://rawofnature.com/demfile/sn/wp/wp-content/plugins/mailchimp//css/flick/flick.css?ver=3.8.1" type="text/css" media="all">
-	<link rel="stylesheet" id="mailchimpSF_main_css-css" href="http://rawofnature.com/demfile/sn/wp/?mcsf_action=main_css&amp;ver=3.8.1" type="text/css" media="all">
-	<!--[if IE]>
-		<link rel='stylesheet' id='mailchimpSF_ie_css-css'  href='http://rawofnature.com/demfile/sn/wp/wp-content/plugins/mailchimp/css/ie.css?ver=3.8.1' type='text/css' media='all' />
-	<![endif]-->
-	<link rel="stylesheet" id="contact-form-7-css" href="http://rawofnature.com/demfile/sn/wp/wp-content/plugins/contact-form-7/includes/css/styles.css?ver=3.5.4" type="text/css" media="all">
-	<link rel="stylesheet" id="ff_shortcodes-css" href="http://rawofnature.com/demfile/sn/wp/wp-content/plugins/ffPackeryGallery/style_pack.css?ver=3.8.1" type="text/css" media="all">
-	<link rel="stylesheet" id="rs-settings-css" href="http://rawofnature.com/demfile/sn/wp/wp-content/plugins/revslider/rs-plugin/css/settings.css?rev=4.0.4&amp;ver=3.8.1" type="text/css" media="all">
-	<link rel="stylesheet" id="rs-captions-css" href="http://rawofnature.com/demfile/sn/wp/wp-content/plugins/revslider/rs-plugin/css/dynamic-captions.css?rev=4.0.4&amp;ver=3.8.1" type="text/css" media="all">
-	<link rel="stylesheet" id="rs-plugin-static-css" href="http://rawofnature.com/demfile/sn/wp/wp-content/plugins/revslider/rs-plugin/css/static-captions.css?rev=4.0.4&amp;ver=3.8.1" type="text/css" media="all">
-	<link rel="stylesheet" id="ff_stylesheet-css" href="http://rawofnature.com/demfile/sn/wp/wp-content/themes/sentinel/style.css?ver=3.8.1" type="text/css" media="all">
-	<style type="text/css">
-		body{ 
-			font-family: 'Open Sans', Arial, sans-serif !important;
-		}
-		input, 
-		textarea, 
-		.search_top_overlay input[type=text], 
-		.blog-post-1_container.post_format_quote .featured_quote .quote_author, 
-		.blockquote-2 .blockquote_author{ 
-			font-family: 'Open Sans', Arial, sans-serif;
-		}
-		.navigation-1 .top-menu-item-a,
-		.navigation-2 a{ 
-			font-family: Helvetica, sans-serif, Arial, sans-serif;
-		}
-		blockquote,
-		.blog-post-1_container.post_format_quote .featured_quote,
-		.testimonial-2 .testimonial_content,
-		.testimonial-3 .testimonial_content,
-		.blockquote-2,
-		.latest_tweet .latest_tweet_text,
-		.footer-social .timeline{ 
-			font-family: Georgia, serif, Arial, sans-serif;
-		}
-		body.layout_boxed{ 
-			background-color: #F5D29D;
-		}
-		body.layout_boxed{ 
-			background-image: url(http://rawofnature.com/demfile/sn/wp/wp-content/themes/sentinel/images/boxed_wood.jpg);
-			background-size: auto;
-			background-position: left top;
-			background-repeat: repeat;
-		}
-	</style>
-	<link rel="stylesheet" id="ff_retina-css" href="http://rawofnature.com/demfile/sn/wp/wp-content/themes/sentinel/retina.css?ver=3.8.1" type="text/css" media="all">
-	<link rel="stylesheet" id="ff_responsive-css" href="http://rawofnature.com/demfile/sn/wp/wp-content/themes/sentinel/responsive.css?ver=3.8.1" type="text/css" media="all">
-	<link rel="stylesheet" id="google_font_Open_Sans-css" href="http://fonts.googleapis.com/css?family=Open+Sans%3A400%2C400italic%2C600%2C600italic%2C700%2C700italic%2C900%2C900italic&amp;subset=latin%2Cvietnamese%2Cgreek%2Ccyrillic-ext%2Clatin-ext%2Ccyrillic%2Cgreek-ext&amp;ver=3.8.1" type="text/css" media="all">
-	<link rel="stylesheet" id="ff_skin-css" href="http://rawofnature.com/demfile/sn/wp/wp-content/themes/sentinel/skins/blue/blue.css?ver=3.8.1" type="text/css" media="all">
-	<link rel="stylesheet" id="ff_bxslider-css" href="http://rawofnature.com/demfile/sn/wp/wp-content/themes/sentinel/scripts/bxslider/jquery.bxslider.css?ver=3.8.1" type="text/css" media="all">
-	<link rel="stylesheet" id="ff_settings-css" href="http://rawofnature.com/demfile/sn/wp/wp-content/themes/sentinel/scripts/rs-plugin/css/settings.css?ver=3.8.1" type="text/css" media="all">
-	<link rel="stylesheet" id="ff_magnific_popup-css" href="http://rawofnature.com/demfile/sn/wp/wp-content/themes/sentinel/scripts/magnific-popup/magnific-popup.css?ver=3.8.1" type="text/css" media="all">
-	<link rel="stylesheet" id="ff_magnific_popup_animations-css" href="http://rawofnature.com/demfile/sn/wp/wp-content/themes/sentinel/scripts/magnific-popup/magnific-popup-anim.css?ver=3.8.1" type="text/css" media="all">
-	<link rel="stylesheet" id="ff_font_awesome-css" href="http://rawofnature.com/demfile/sn/wp/wp-content/themes/sentinel/scripts/fontawesome/css/font-awesome.min.css?ver=3.8.1" type="text/css" media="all">
-	<link rel="stylesheet" id="ff_entyp-css" href="http://rawofnature.com/demfile/sn/wp/wp-content/themes/sentinel/scripts/entypo/entypo.css?ver=3.8.1" type="text/css" media="all">
-	<link rel="stylesheet" id="ff_zocial-css" href="http://rawofnature.com/demfile/sn/wp/wp-content/themes/sentinel/scripts/zocial/zocial.css?ver=3.8.1" type="text/css" media="all">
-	<link rel="stylesheet" id="style-switcher-css" href="http://rawofnature.com/demfile/sn/wp/wp-content/themes/sentinel/framework/demo/switcher.css?ver=3.8.1" type="text/css" media="all">
-	<link rel="stylesheet" id="color-picker-css" href="http://rawofnature.com/demfile/sn/wp/wp-content/themes/sentinel/scripts/colorpicker/css/colorpicker.css?ver=3.8.1" type="text/css" media="all">
-	
-	<script type="text/javascript" src="http://rawofnature.com/demfile/sn/wp/wp-includes/js/jquery/jquery.js?ver=1.10.2"></script>
-	<script type="text/javascript" src="http://rawofnature.com/demfile/sn/wp/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1"></script>
-	<script type="text/javascript" src="http://rawofnature.com/demfile/sn/wp/wp-content/plugins/mailchimp/js/scrollTo.js?ver=1.4.1"></script>
-	<script type="text/javascript" src="http://rawofnature.com/demfile/sn/wp/wp-content/plugins/contact-form-7/includes/js/jquery.form.min.js?ver=3.45.0-2013.10.17"></script>
-	<script type="text/javascript">
-		/* <![CDATA[ */
-		var mailchimpSF = {"ajax_url":"http:\/\/rawofnature.com\/demfile\/sn\/wp\/"};
-		/* ]]> */
-	</script>
-	<script type="text/javascript" src="http://rawofnature.com/demfile/sn/wp/wp-content/plugins/mailchimp/js/mailchimp.js?ver=1.4.1"></script>
-	<script type="text/javascript" src="http://rawofnature.com/demfile/sn/wp/wp-includes/js/jquery/ui/jquery.ui.core.min.js?ver=1.10.3"></script>
-	<script type="text/javascript" src="http://rawofnature.com/demfile/sn/wp/wp-content/plugins/mailchimp//js/datepicker.js?ver=3.8.1"></script>
-	<script type="text/javascript">
-		/* <![CDATA[ */
-		var dot_irecommendthis = {"ajaxurl":"http:\/\/rawofnature.com\/demfile\/sn\/wp\/wp-admin\/admin-ajax.php"};
-		/* ]]> */
-	</script>
-	<script type="text/javascript" src="http://rawofnature.com/demfile/sn/wp/wp-content/plugins/i-recommend-this/js/dot_irecommendthis.js?ver=2.6.0"></script>
-	<script type="text/javascript" src="http://rawofnature.com/demfile/sn/wp/wp-content/plugins/revslider/rs-plugin/js/jquery.themepunch.plugins.min.js?rev=4.0.4&amp;ver=3.8.1"></script>
-	<script type="text/javascript" src="http://rawofnature.com/demfile/sn/wp/wp-content/plugins/revslider/rs-plugin/js/jquery.themepunch.revolution.min.js?rev=4.0.4&amp;ver=3.8.1"></script>
-	<script type="text/javascript" src="http://rawofnature.com/demfile/sn/wp/wp-content/themes/sentinel/scripts/bxslider/jquery.bxslider.min.js?ver=3.8.1"></script>
-	<script type="text/javascript" src="http://rawofnature.com/demfile/sn/wp/wp-content/themes/sentinel/scripts/caroufredsel/jquery.caroufredsel.min.js?ver=3.8.1"></script>
-	<script type="text/javascript" src="http://rawofnature.com/demfile/sn/wp/wp-content/themes/sentinel/scripts/colorpicker/js/colorpicker.js?ver=3.8.1"></script>
-	
-	<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://rawofnature.com/demfile/sn/wp/xmlrpc.php?rsd">
-	<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://rawofnature.com/demfile/sn/wp/wp-includes/wlwmanifest.xml"> 
-	<link rel="prev" title="Gallery" href="http://rawofnature.com/demfile/sn/wp/gallery/">
-	<link rel="next" title="Blog" href="http://rawofnature.com/demfile/sn/wp/blog/">
-	
-	<meta name="generator" content="Mediateq">
-	<link rel="canonical" href="http://rawofnature.com/demfile/sn/wp/">
-	<link rel="shortlink" href="http://rawofnature.com/demfile/sn/wp/?p=675">
-	<script type="text/javascript">
-		jQuery(function($) {
-			$('.date-pick').each(function() {
-				var format = $(this).data('format') || 'mm/dd/yyyy';
-				format = format.replace(/yyyy/i, 'yy');
-				$(this).datepicker({
-					autoFocusNextInput: true,
-					constrainInput: false,
-					changeMonth: true,
-					changeYear: true,
-					beforeShow: function(input, inst) { $('#ui-datepicker-div').addClass('show'); },
-					dateFormat: format.toLowerCase(),
-				});
-			});
-			d = new Date();
-			$('.birthdate-pick').each(function() {
-				var format = $(this).data('format') || 'mm/dd';
-				format = format.replace(/yyyy/i, 'yy');
-				$(this).datepicker({
-					autoFocusNextInput: true,
-					constrainInput: false,
-					changeMonth: true,
-					changeYear: false,
-					minDate: new Date(d.getFullYear(), 1-1, 1),
-					maxDate: new Date(d.getFullYear(), 12-1, 31),
-					beforeShow: function(input, inst) { $('#ui-datepicker-div').removeClass('show'); },
-					dateFormat: format.toLowerCase(),
-				});
-
-			});
-
-		});
-	</script>
-	<style type="text/css">
-		.recentcomments a{
-			display:inline !important;
-			padding:0 !important;
-			margin:0 !important;
-		}
-	</style>
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', 'UA-43311365-2', 'rawofnature.com');
-	  ga('send', 'pageview');
-	</script>
-</head>
-<body class="home page page-id-675 page-template-default layout_fullwidth wpb-js-composer js-comp-ver-3.6.14.1 vc_responsive">
+<?php 
+	include_once("./themes/inc/header.php");
+?>
 	<div id="layout_width" class="fullwidth">
 		<div class="content_container">
 			<div class="header-2_container cwidth_container">
@@ -1565,7 +1417,7 @@
 				</div><!-- END BOXED -->
 				<!-- Style switcher
 				================================================== -->
-				<div class="styleSwitcherWrapper">
+				<!-- <div class="styleSwitcherWrapper">
 					<div class="styleSwitcherPanel">
 					    <h4>BASIC OPTIONS</h4>				
 						<p class="skin_color_section">Skin Color <span style=""></span></p>
@@ -1660,7 +1512,7 @@
 					<a href="" class="styleSwitcherToggle">
 						<span class="styleSwitcherToggleImage"></span>
 					</a>
-				</div>
+				</div> -->
 				<!-- Style switcher END
 				================================================== -->
 				<script>
