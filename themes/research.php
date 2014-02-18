b6fa96465a05116a09b713409beaac2281dc5b72
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2014-02-18 12:22:34 +0330

    add research page

--- themes/research.php
+++ themes/research.php
@@ -1,3 +1,68 @@
 <?php
-	return "lfjglkjflgjlfg";
+$html=<<<cd
+	<div class="top_content">
+		<div class="main_content_container cwidth_container">
+			<div class="main_content_wrapper cwidth_wrapper">
+				<div class="main_content cwidth">
+					<div class="col-1-1">
+						<div class="col">
+							<div class="grid">
+								<div class="col-content">
+									<div class="col">
+										<article id="post-192" class="post-192 portfolio type-portfolio status-publish hentry tag-webdesign portfolio-single-1_container main_column">
+											<div class="portfolio-single-1_wrapper">
+												<div class="portfolio-single-1 content_cols_eq">
+													<div class="portfolio_titlebar-2 clearfix">
+														<div class="portfolio_titlebar_right">
+															<h2 class="post_title">تحقیقات</h2>
+														</div>
+														<!-- <div class="portfolio_titlebar_right">
+															<div class="portfolio_nav">
+																<a href="#" class="portfolio_prev"></a>
+																<a href="#" class="portfolio_category"></a>			
+																<a href="#" class="portfolio_next"></a>
+															</div>
+														</div> -->
+													</div>
+													<div class="grid">
+														<div class="col-portfolio-single-left" style="width:70%;">
+															<div class="col">
+																<div class="featured_area">
+																	<ul class="bxslider">
+																		<li><img src="http://rawofnature.com/demfile/sn/wp/wp-content/uploads/freshizer/80c9ef192567f5c0682735eb3644ea2a_keyb-770-480-c.jpg" data-at2x="http://rawofnature.com/demfile/sn/wp/wp-content/uploads/freshizer/80c9ef192567f5c0682735eb3644ea2a_keyb-770-480-c@2x.jpg" alt="" data-disable-retina-attrs-resize="false"></li>
+																		<li><img src="http://rawofnature.com/demfile/sn/wp/wp-content/uploads/freshizer/98e5ccfb1f8835e8b4ecf7590385e19b_women-770-480-c.jpg" data-at2x="http://rawofnature.com/demfile/sn/wp/wp-content/uploads/freshizer/98e5ccfb1f8835e8b4ecf7590385e19b_women-770-480-c@2x.jpg" alt="" data-disable-retina-attrs-resize="false"></li>
+																	</ul>
+																</div>
+															</div>
+														</div>
+														<div class="col-portfolio-single-right" style="width:30%;">
+															<div class="col">
+																<div class="post_meta clearfix">
+																	<div class="post_meta_item post_date">Posted: <strong>November 18, 2013</strong></div>
+																	<span class="post_meta_item like_number">
+																		<a href="#" class="dot-irecommendthis" id="dot-irecommendthis-192" title="Recommend this"><span class="dot-irecommendthis-count">47</span> <span class="dot-irecommendthis-suffix"></span></a>
+																	</span>
+																</div>
+																<div class="post_content">
+																	<p>Perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque.</p>
+																	<p>Laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+																	<h3>Project Details</h3><table class="table-3"><tbody><tr><td>CLIENT:</td><td></td><td>ThemeForest</td><td></td></tr><tr><td>DATE:</td><td></td><td>April 11, 2013</td><td></td></tr><tr><td>TAGS:</td><td></td><td>Animation, Web Development</td><td></td></tr></tbody></table><a href="http://www.google.com/" class="sc_button sc_button_round sc_button_small sc_button_dark">Launch Project</a>
+																</div>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</article>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+cd;
+return $html;
 ?>
