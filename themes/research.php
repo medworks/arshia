f977deca6cb305ce6a9f885283c19bb0bf90aced
mojiAm <amjadi.mojtaba@gmail.com>
2014-02-19 12:28:59 -0800

    add persian text

--- themes/research.php
+++ themes/research.php
@@ -47,8 +47,7 @@ $html=<<<cd
 																	</span>
 																</div> -->
 																<div class="post_content">
-																	<p>Perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque.</p>
-																	<p>Laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+																	<p>توضیحات</p>
 																	<!-- <h3>Project Details</h3><table class="table-3"><tbody><tr><td>CLIENT:</td><td></td><td>ThemeForest</td><td></td></tr><tr><td>DATE:</td><td></td><td>April 11, 2013</td><td></td></tr><tr><td>TAGS:</td><td></td><td>Animation, Web Development</td><td></td></tr></tbody></table><a href="http://www.google.com/" class="sc_button sc_button_round sc_button_small sc_button_dark">Launch Project</a> -->
 																</div>
 															</div>
