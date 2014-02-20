358810da01da306e72a64a64424091eb54189a2d
mojiAm <amjadi.mojtaba@gmail.com>
2014-02-20 11:49:28 -0800

    del somth

--- themes/inc/index-content.php
+++ themes/inc/index-content.php
@@ -240,7 +240,7 @@ $html=<<<cd
 															<div class="grid ">
 																<div class="col-1-1">
 																	<div class="col">
-																		<h3 style="text-align:right;">رزومه</h3>
+																		<h3 style="text-align:right;"></h3>
 																	</div>
 																</div>
 															</div>
@@ -422,169 +422,20 @@ $html=<<<cd
 																	</div>
 																</div>
 															</div>
-															<div class="grid ">
-																<div class="col-1-2 ">
-																	<div class="col">
-																		<div class="recent_posts_list">
-																			<h3>آخرین اخبار</h3>
-																			<div class="rpl_post ">
-																				<div class="blog-post-1">
-																					<div class="grid">
-																						<div class="col-blog-post-left">
-																							<div class="col">
-																								<div class="post_meta_left">
-																									<div class="post_meta_left_date_wrapper">
-																										<div class="post_meta_left_date">
-																											<div class="date_box">21</div>
-																										</div>
-																										<div class="date_month">تیر</div>
-																									</div>
-																								</div>
-																							</div>
-																						</div>
-																						<div class="col-blog-post-right">
-																							<div class="col">
-																								<div class="post_header">
-																									<h2 class="post_title">
-																										<a href="#">خبر شماه دو</a>
-																									</h2>
-																								</div>
-																								<!-- <div class="post_meta">
-																									<span class="post_meta_item post_meta_comment_number">
-																										<a href="http://rawofnature.com/demfile/sn/wp/this-is-a-standard-post/">No Comments</a>
-																									</span> 
-																								</div> -->
-																								<div class="post_content">
-																									<p>این سایت کاملا ریسپانسیو بوده و از آخرین متدهای رنکینگ و بالا بردن سئو در تمام موتورهای جستجو در آن استفاده شده ایت. <a href="#" class="read_more_link">ادامه مطلب</a></p>
-																								</div>
-																							</div>
-																						</div>
-																					</div>
-																				</div>
-																			</div>
-																			<div class="rpl_post ">
-																				<div class="blog-post-1">
-																					<div class="grid">
-																						<div class="col-blog-post-left">
-																							<div class="col">
-																								<div class="post_meta_left">
-																									<div class="post_meta_left_date_wrapper">
-																										<div class="post_meta_left_date">
-																											<div class="date_box">19</div>
-																										</div>
-																										<div class="date_month">آذر</div>
-																									</div>
-																								</div>
-																							</div>
-																						</div>
-																						<div class="col-blog-post-right">
-																							<div class="col">
-																								<div class="post_header">
-																									<h2 class="post_title">
-																										<a href="#">خبر شماه یک</a>
-																									</h2>
-																								</div>
-																								<!-- <div class="post_meta">
-																									<span class="post_meta_item post_meta_comment_number">
-																										<a href="http://rawofnature.com/demfile/sn/wp/this-is-a-standard-post/">No Comments</a>
-																									</span> 
-																								</div> -->
-																								<div class="post_content">
-																									<p>این سایت کاملا ریسپانسیو بوده و از آخرین متدهای رنکینگ و بالا بردن سئو در تمام موتورهای جستجو در آن استفاده شده ایت. <a href="#" class="read_more_link">ادامه مطلب</a></p>
-																								</div>
-																							</div>
-																						</div>
-																					</div>
-																				</div>
-																			</div>
-																		</div>
-																	</div>
-																</div>
-																<div class="col-1-4 ">
-																	<div class="col">
-																		<h3>بیشتر درباره ما بدانید</h3>
-																		<div class="accordeon-2">
-																			<div class="tb_accordeon_container">
-																				<div class="tb_accordeon_wrapper">
-																					<div class="tb_accordeon">
-																						<div class="tb_accordeon_item ">
-																							<div class="tb_accordeon_item_post_title_wrapper">
-																								<div class="tb_accordeon_item_arrows"></div>
-																								<h3 class="tb_accordeon_item_title">ما که هستیم؟</h3>
-																								<div class="clear"></div>
-																							</div>
-																							<div class="tb_accordeon_item_content_wrapper" style="display: block; height: 0px;">
-																								<div class="tb_accordeon_item_content">
-																									<div class="">
-																										<p>درباره ما... </p>
-																									</div>
-																								</div>
-																							</div>
-																						</div>
-																						<div class="tb_accordeon_item tb_accordeon_item_open">
-																							<div class="tb_accordeon_item_post_title_wrapper">
-																								<div class="tb_accordeon_item_arrows"></div>
-																								<h3 class="tb_accordeon_item_title">ما چه میکنیم؟</h3>
-																								<div class="clear"></div>
-																							</div>
-																							<div class="tb_accordeon_item_content_wrapper" style="display: block; height: 0px;">
-																								<div class="tb_accordeon_item_content">
-																									<div class="">
-																										<p>کار ما... </p>
-																									</div>
-																								</div>
-																							</div>
-																						</div>
-																						<div class="tb_accordeon_item ">
-																							<div class="tb_accordeon_item_post_title_wrapper">
-																								<div class="tb_accordeon_item_arrows"></div>
-																								<h3 class="tb_accordeon_item_title">ما کجا هستیم؟</h3>
-																								<div class="clear"></div>
-																							</div>
-																							<div class="tb_accordeon_item_content_wrapper" style="display: block; height: 0px;">
-																								<div class="tb_accordeon_item_content">
-																									<div class="">
-																										<p>آدرس ما..</p>
-																									</div>
-																								</div>
-																							</div>
-																						</div>
-																					</div>
-																				</div>
-																			</div>
-																		</div>
-																	</div>
-																</div>
-																<div class="col-1-4 ">
-																	<div class="col">
-																		<h3>از زبان مشتریان</h3>
-																		<div class="testimonial-2 ">
-																			<div class="testimonial_content">
-																				<div class="testimonial_content_inner">
-																					<p>کامنت های مربوط به مشتریان... این سایت کاملا ریسپانسیو بوده و در آن از آخرین متدهای رنکینگ و سئو برای بالا بردن سایت در موتورهای جستجو مانند گوگل استفاده شده است.</p>
-																				</div>
-																			</div>
-																			<!-- <div class="testimonial_author clearfix"><img class="testimonial_photo" src="http://rawofnature.com/demfile/sn/wp/wp-content/uploads/freshizer/acd1f94ba73128cb74463e8c2ff04719_head1-40-40-c.jpg" alt=""><br>
-																				<h3 class="testimonial_name">Robert Green</h3>
-																				<div class="testimonial_position">Businessman</div>
-																			</div> -->
-																		</div>
-																	</div>
-																</div>
-															</div>
-														</div>																							</div>
+														</div>
 													</div>
-												</article>
-											</div>
+												</div>
+											</article>
 										</div>
-										<!-- No sidebar -->
 									</div>
+									<!-- No sidebar -->
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
+		</div>
 cd;
 echo $html;
 }
