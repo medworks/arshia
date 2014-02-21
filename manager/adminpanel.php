7b800abc07e1b8785cbc4420a1769dbdb6797067
Saeid Hatami <Hatami4510@gmail.com>
2014-02-21 19:15:12 +0330

    remove some utility

--- manager/adminpanel.php
+++ manager/adminpanel.php
@@ -103,28 +103,12 @@
           <li>
             <a href="?item=gallerymgr&act=do" class="gallerymgr"><p>گالری تصاویر</p></a>
           </li>
-    		  <li>
-            <a href="?item=blocksmgr&act=do" class="block"><p>مدیریت بلاک</p></a>
-          </li>
-    		  <li>
-            <a href="?item=pluginmgr&act=do" class="plugin"><p>مدیریت پلاگین</p></a>
-          </li>
-		      <li >            
-            <a href="?item=newslettermgr&act=do" id="newslettermgr" class="newslettermgr"><p>مدیریت خبرنامه</p></a>
-          </li>
-		  </li>
-		      <li >            
-            <a href="?item=pollmgr&act=do" id="pollmgr" class="pollmgr"><p>نظرسنجی</p></a>
-          </li>
     	    <li >            
             <a href="?item=usermgr&act=do" id="users" name="users" class="users"><p>مدیریت کاربران</p></a>
           </li>	
           <li >            
             <a href="?item=uploadmgr&act=do" id="users" name="users" class="upload"><p>مدیریت آپلود</p></a>
-          </li>		  
-          <li>
-            <a href="?item=thememgr&act=do" class="template"><p>مدیریت قالب</p></a>
-          </li>
+          </li>		           
   		    <li>
             <a href="?item=settingmgr&act=do" class="setting"><p>تنظیمات</p></a>
           </li>
