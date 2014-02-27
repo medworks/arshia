13b2d92c4173d28ca7e283effc6ebcc0189bfbcd
Saeid Hatami <hatami4510@gmail.com>
2014-02-27 12:20:41 +0330

    change some codes & fix some errors

--- manager/requestmgr.php
+++ manager/requestmgr.php
@@ -14,106 +14,22 @@
  }
  if ($_GET['item']!="requestmgr")	exit();
  $db = Database::GetDatabase();
- $overall_error = false;
- $pic_on_edit_section = null;
- if (isset($_POST["mark"]) and $_POST["mark"]!="srhgall")
- {
-	 if(empty($_POST["selectpic"]))
-		{ 
-			//$msgs = $msg->ShowError("لط??ا ??ایل عکس را انتخاب کنید");
-			header('location:?item=gallermgr&act=new&msg=4');
-			//$_GET["item"] = "gallermgr";
-			//$_GET["act"] = "new";
-			//$_GET["msg"] = 4;
-			$overall_error = true;
-			//exit();
-		}
-  } 	
- if (!$overall_error && $_POST["mark"]=="savegall")
- {						   				
-	$fields = array("`image`","`subject`","`body`");	
-	$values = array("'{$_POST[selectpic]}'","'{$_POST[subject]}'","'{$_POST[body]}'");
-	if (!$db->InsertQuery('gallery',$fields,$values)) 
-	{
-		//$msgs = $msg->ShowError("ثبت اطلاعات با مشکل مواجه شد");
-		header('location:?item=gallerymgr&act=new&msg=2');
-		//exit();
-		//$_GET["item"] = "gallerymgr";
-		//$_GET["act"] = "new";
-		//$_GET["msg"] = 2;
-	} 	
-	else 
-	{  										
-		//$msgs = $msg->ShowSuccess("ثبت اطلاعات با موفقیت انجام شد");
-		header('location:?item=gallerymgr&act=new&msg=1');					
-		//exit();
-		//$_GET["item"] = "gallerymgr";
-		//$_GET["act"] = "new";
-		//$_GET["msg"] = 1;
-	 }
- }
- else
- if (!$overall_error && $_POST["mark"]=="editgall")
- {			    
-	$values = array("`image`"=>"'{$_POST[selectpic]}'",
-	       		    "`subject`"=>"'{$_POST[subject]}'",
-					"`body`"=>"'{$_POST[body]}'");
-	$db->UpdateQuery("gallery",$values,array("id='{$_GET['sid']}'"));
-	header('location:?item=gallerymgr&act=mgr');
-	//$_GET["item"] = "gallerymgr";
-	//$_GET["act"] = "mgr";			
- }
-
-	if ($overall_error)
-	{
-		$row = array("image"=>$_FILES['pic']['name'],
-					 "subject"=>$_POST['subject'],
-					 "body"=>$_POST['body']);
-	}
  
-   if ($_GET['act']=="new")
-	{
-	    $pic_on_edit_insert_section ="<img id='img' src='' alt='' />";
-		$editorinsert = "
-			<p>
-				<input type='submit' id='submit' value='ذخیره' class='submit' />	 
-				<input type='hidden' name='mark' value='savegall' />";
-	}
-	if ($_GET['act']=="edit")
-	{
-		$row=$db->Select("gallery","*","id='{$_GET["sid"]}'",NULL);
-		$pic_on_edit_insert_section = "<img src='{$row[image]}'width='200px' height='100px' />";
-		$editorinsert = "
-		<p>
-			 <input type='submit' id='submit' value='ویرایش' class='submit' />	 
-			 <input type='hidden' name='mark' value='editgall' />";
-	}
-	if ($_GET['act']=="del")
-	{
-		$db->Delete("gallery"," id",$_GET["sid"]);
-		if ($db->CountAll("gallery")%10==0) $_GET["pageNo"]-=1;		
-		header("location:?item=gallerymgr&act=mgr&pageNo={$_GET[pageNo]}");
-	}	
-if ($_GET['act']=="do")
+ if ($_GET['act']=="do")
 {
 	$html=<<<ht
 		<div class="title">
 	      <ul>
 	        <li><a href="adminpanel.php?item=dashboard&act=do">پیشخوان</a></li>
-	        <li><span>گالری تصاویر</span></li>
+	        <li><span>مدیریت درخواست مستندات</span></li>
 	      </ul>
 	      <div class="badboy"></div>
 	    </div>
 		<div class="sub-menu" id="mainnav">
-			<ul>
-			  <li>		  
-				<a href="?item=gallerymgr&act=new">عکس جدید
-					<span class="add-gallery"></span>
-				</a>
-			  </li>
+			<ul>			  
 			  <li>
-				<a href="?item=gallerymgr&act=mgr" id="news" name="news">
-				  حذف/ویرایش عکسها
+				<a href="?item=requestmgr&act=mgr" id="request" name="request">
+				  مدیریت درخواست ها
 					<span class="edit-gallery"></span>
 				</a>
 			  </li>
@@ -122,62 +38,12 @@ if ($_GET['act']=="do")
 		</div>		 
 ht;
 }else
-if ($_GET['act']=="new" or $_GET['act']=="edit")
-{
-$msgs = GetMessage($_GET['msg']);
-$html=<<<cd
-		<script type='text/javascript'>
-			$(document).ready(function(){		
-				$("#frmgallerymgr").validationEngine();			   
-			});
-		</script>
-		<div class="title">
-	      <ul>
-	        <li><a href="adminpanel.php?item=dashboard&act=do">پیشخوان</a></li>
-	        <li><span>مدیریت عکسها</span></li>
-	      </ul>
-	      <div class="badboy"></div>
-	    </div>	     
-		<div id="message">
-		{$msgs}
-		</div>
-		<form name="frmgallerymgr" id="frmgallerymgr" class="" action="" method="post" enctype="multipart/form-data" > 
-			<p>
-				<label for="pic">عکس </label>
-				<span>*</span>
-			</p>
-			<p>
-		   		<input type="text" name="selectpic" class="selectpic" id="selectpic" value='{$row[image]}' />
-		   		<input type="text" class="validate[required] showadd" id="showadd" value='{$row[image]}' />
-		   		<a class="filesbrowserbtn" id="filesbrowserbtn" name="gallerymgr" title="گالری تصاویر">گالری تصاویر</a>
-		   		<a class="selectbuttton" id="selectbuttton" title="انتخاب">انتخاب</a>
-		   </p>
-		   <div class="badboy"></div>
-		   <div id="filesbrowser"></div>
-		   <div class="badboy"></div>
-			<p>
-				<label for="subject">عنوان </label>
-				<span>*</span>
-			</p>
-			<input type="text" name="subject" class="validate[required] subject" id="subject" value="{$row[subject]}" />
-			<p>
-				<label for="subject">توضیحات </label>
-				<span></span>
-			</p>
-			<input type="text" name="body" class="subject" id="body" value="{$row[body]}" /> 
-			{$editorinsert}
-				<input type="reset" value="پاک کردن" class='reset' /> 				
-			</p>
-		</form>
-cd;
-}
-else
 if ($_GET['act']=="mgr")
 {
-	if ($_POST["mark"]=="srhgall")
+	if ($_POST["mark"]=="srhreq")
 	{	 			   
 		$rows = $db->SelectAll(
-				"gallery",
+				"request",
 				"*",
 				"{$_POST[cbsearch]} LIKE '%{$_POST[txtsrh]}%'",
 				"id DESC",
@@ -188,14 +54,14 @@ if ($_GET['act']=="mgr")
 				//$_GET['item'] = "gallery";
 				//$_GET['act'] = "mgr";
 				//$_GET['msg'] = 6;				
-				header("Location:?item=worksmgr&act=mgr&msg=6");
+				header("Location:?item=requestmgr&act=mgr&msg=6");
 			}
 		
 	}
 	else
 	{	
 		$rows = $db->SelectAll(
-				"gallery",
+				"request",
 				"*",
 				null,
 				"id DESC",
@@ -204,14 +70,9 @@ if ($_GET['act']=="mgr")
     }
                 $rowsClass = array();
                 $colsClass = array();
-                $rowCount =($_GET["rec"]=="all" or $_POST["mark"]!="srhgall" )?$db->CountAll("gallery"):Count($rows);
+                $rowCount =($_GET["rec"]=="all" or $_POST["mark"]!="srhreq" )?$db->CountAll("request"):Count($rows);
                 for($i = 0; $i < Count($rows); $i++)
-                {						
-		        $rows[$i]["subject"] =(mb_strlen($rows[$i]["subject"])>20)?mb_substr($rows[$i]["subject"],0,20,"UTF-8")."...":$rows[$i]["subject"];
-                $rows[$i]["body"] =(mb_strlen($rows[$i]["body"])>30)?
-                mb_substr(html_entity_decode(strip_tags($rows[$i]["body"]), ENT_QUOTES, "UTF-8"), 0, 30,"UTF-8") . "..." :
-                html_entity_decode(strip_tags($rows[$i]["body"]), ENT_QUOTES, "UTF-8");               
-                $rows[$i]["image"] ="<img src='{$rows[$i][image]}' alt='{$rows[$i][subject]}' width='40px' height='40px' />";
+                {								        
 				if ($i % 2==0)
 				 {
 						$rowsClass[] = "datagridevenrow";
@@ -220,13 +81,13 @@ if ($_GET['act']=="mgr")
 				{
 						$rowsClass[] = "datagridoddrow";
 				}				
-				$rows[$i]["edit"] = "<a href='?item=gallerymgr&act=edit&sid={$rows[$i]["id"]}' class='edit-field'" .
+				$rows[$i]["edit"] = "<a href='?item=requestmgr&act=edit&rid={$rows[$i]["id"]}' class='edit-field'" .
 						"style='text-decoration:none;'></a>";								
 				$rows[$i]["delete"]=<<< del
 				<a href="javascript:void(0)"
 				onclick="DelMsg('{$rows[$i]['id']}',
 					'از حذف این فعالیت اطمینان دارید؟',
-				'?item=gallerymgr&act=del&pageNo={$_GET[pageNo]}&sid=');"
+				'?item=requestmgr&act=del&pageNo={$_GET[pageNo]}&rid=');"
 				 class='del-field' style='text-decoration:none;'></a>
 del;
                }
@@ -235,17 +96,22 @@ del;
             if (Count($rows) > 0)
             {                    
                     $gridcode.= DataGrid(array( 
-							"image"=>"عکس",
-							"subject"=>"عنوان",
-							"body"=>"توضیحات",
-							 "edit"=>"ویرایش",
+							"name"=>"نام",
+							"tel"=>"تلفن",
+							"mobile"=>"موبایل",
+							"address"=>"آدرس",
+							"postcode"=>"کد پستی",
+							"email"=>"ایمیل",
+							"edit"=>"ویرایش",
 							"delete"=>"حذف",), $rows, $colsClass, $rowsClass, 10,
-                            $_GET["pageNo"], "id", false, true, true, $rowCount,"item=gallerymgr&act=mgr");
+                            $_GET["pageNo"], "id", false, true, true, $rowCount,"item=requestmgr&act=mgr");
                     
             }
 $msgs = GetMessage($_GET['msg']);
-$list = array("subject"=>"عنوان",
-              "body"=>"توضیحات" );
+$list = array("name"=>"عنوان",
+              "tel"=>"تلفن",
+			  "mobile"=>"موبایل",
+			  "email"=>"ایمیل");
 $combobox = SelectOptionTag("cbsearch",$list,"subject");
 $code=<<<edit
 <script type='text/javascript'>
@@ -259,23 +125,21 @@ $code=<<<edit
 					<div class="title">
 				      <ul>
 				        <li><a href="adminpanel.php?item=dashboard&act=do">پیشخوان</a></li>
-					    <li><span>مدیریت اسلاید</span></li>
+					    <li><span>مدیریت درخواست مستندات</span></li>
 				      </ul>
 				      <div class="badboy"></div>
 				  </div>
                     <div class="Top">                       
 						<center>
-							<form action="?item=gallerymgr&act=mgr" method="post" id="frmsrh" name="frmsrh">
+							<form action="?item=requestmgr&act=mgr" method="post" id="frmsrh" name="frmsrh">
 								<p>جستجو بر اساس {$combobox}							
 									<input type="text" name="txtsrh" class="search-form" value="جستجو..." onfocus="if (this.value == 'جستجو...') {this.value = '';}" onblur="if (this.value == '') {this.value = 'جستجو...';}"  />
-									<a href="?item=gallerymgr&act=mgr" name="srhsubmit" id="srhsubmit" class="button"> جستجو</a>
-									<a href="?item=gallerymgr&act=mgr&rec=all" name="retall" id="retall" class="button"> کلیه اطلاعات</a>
+									<a href="?item=requestmgr&act=mgr" name="srhsubmit" id="srhsubmit" class="button"> جستجو</a>
+									<a href="?item=requestmgr&act=mgr&rec=all" name="retall" id="retall" class="button"> کلیه اطلاعات</a>
 								</p>
-								<input type="hidden" name="mark" value="srhgall" /> 
+								<input type="hidden" name="mark" value="srhreq" /> 
 								{$msgs}
-
 								{$gridcode} 
-															
 							</form>
 					   </center>
 					</div>
