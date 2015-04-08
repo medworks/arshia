22ff0d0758380798176e7c79eae8654a50b09019
mojiAm <amjadi.mojtaba@gmail.com>
2015-04-08 11:06:32 +0330

    fix style

--- manager/uploadmgr.php
+++ manager/uploadmgr.php
@@ -152,7 +152,7 @@ if ($_GET['act']=="do")
 	      <div class="badboy"></div>
 	    </div>
 		<div class="sub-menu" id="mainnav">
-			<ul class="two-column">
+			<ul>
 			  <li>		  
 				<a href="?item=uploadmgr&act=new">عکس جدید
 					<span class="add-file"></span>
